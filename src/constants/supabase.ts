fa2f6d10519ab25b6bd39e2ec6a47f792996b17c
Busra <kocaidestek@gmail.com>
2026-06-03 21:36:21 +0300

    fix: supabase anon key

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://borviddcrpbsqgivwlzt.supabase.co'
-const supabaseAnonKey = 'sb_publishable_GtvLnLzeaiohSbqiCBPpig_5cD3USz6'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvcnZpZGRjcnBic3FnaXZ3bHp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDU5ODQsImV4cCI6MjA5NTQyMTk4NH0.04ttlYVbKn9u8JCe1hwohMwcrdplLzI_b6CMa1eMO5E'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
